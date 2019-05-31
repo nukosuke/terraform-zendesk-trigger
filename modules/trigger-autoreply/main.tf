5fdd3d338bec7635e4e9fd1148c1e5b9635795bf
nukosuke <nukosuke@lavabit.com>
2019-05-31 19:11:56 +0900

    Add autoreply trigger module

---
+++ modules/trigger-autoreply/main.tf
@@ -0,0 +1,33 @@
+resource "zendesk_trigger" "trigger-autoreply" {
+  title  = "${var.title}"
+  active = "${var.active}"
+
+  all {
+    field    = "role"
+    operator = "is"
+    value    = "end_user"
+  }
+
+  all {
+    field    = "update_type"
+    operator = "is"
+    value    = "Create"
+  }
+
+  all {
+    field    = "status"
+    operator = "is_not"
+    value    = "solved"
+  }
+
+  action {
+    field = "notification_user"
+    value = <<ENVELOPE
+[
+  "requester_id",
+  "${var.subject}",
+  "${var.body}"
+]
+ENVELOPE
+  }
+}
---
+++ modules/trigger-autoreply/variables.tf
@@ -0,0 +1,17 @@
+variable "title" {
+  type    = "string"
+  default = "Auto Reply"
+}
+
+variable "active" {
+  type    = "bool"
+  default = true
+}
+
+variable "subject" {
+  type = "string"
+}
+
+variable "body" {
+  type = "string"
+}
