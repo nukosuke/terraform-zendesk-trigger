0c5a53f554701a8a8e2281f8799be370d19688be
nukosuke <nukosuke@users.noreply.github.com>
2019-11-25 20:12:23 +0900

    Fix main.tf

--- examples/trigger-autoreply/main.tf
+++ examples/trigger-autoreply/main.tf
@@ -1,12 +1,12 @@
 # Configure the Zendesk Provider
-variable "ZENDESK_ACCOUNT" { type = "string" }
-variable "ZENDESK_EMAIL"   { type = "string" }
-variable "ZENDESK_TOKEN"   { type = "string" }
+variable "ZENDESK_ACCOUNT" { type = string }
+variable "ZENDESK_EMAIL"   { type = string }
+variable "ZENDESK_TOKEN"   { type = string }
 
 provider "zendesk" {
-  account = "${var.ZENDESK_ACCOUNT}"
-  email   = "${var.ZENDESK_EMAIL}"
-  token   = "${var.ZENDESK_TOKEN}"
+  account = var.ZENDESK_ACCOUNT
+  email   = var.ZENDESK_EMAIL
+  token   = var.ZENDESK_TOKEN
 }
 
 module "trigger_autoreply" {
--- examples/trigger-turn-counter/main.tf
+++ examples/trigger-turn-counter/main.tf
@@ -1,17 +1,15 @@
 # Configure the Zendesk Provider
-variable "ZENDESK_ACCOUNT" { type = "string" }
-variable "ZENDESK_EMAIL"   { type = "string" }
-variable "ZENDESK_TOKEN"   { type = "string" }
+variable "ZENDESK_ACCOUNT" { type = string }
+variable "ZENDESK_EMAIL"   { type = string }
+variable "ZENDESK_TOKEN"   { type = string }
 
 provider "zendesk" {
-  account = "${var.ZENDESK_ACCOUNT}"
-  email   = "${var.ZENDESK_EMAIL}"
-  token   = "${var.ZENDESK_TOKEN}"
+  account = var.ZENDESK_ACCOUNT
+  email   = var.ZENDESK_EMAIL
+  token   = var.ZENDESK_TOKEN
 }
 
 module "turn-counter" {
-  source = "../../modules/trigger-turn-counter"
-
-  max_count      = 10
-  start_position = 8
+  source    = "../../modules/trigger-turn-counter"
+  max_count = 10
 }
